e92107652a2460263843ae8bb045b869cde19100
king-stronger <114509657+king-stronger@users.noreply.github.com>
2026-01-08 19:30:23 +0000

    ✨ feat: add images and update menu functionality
    - ✨ add audience, challenges, and hero images
    - 🔥 remove theme toggle functionality from main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 const menu = document.getElementById("menu")
-const themeBtn = document.getElementById("theme-button")
 const menuOpenBtn = document.getElementById("open-menu")
 const menuCloseBtn = document.getElementById("close-menu")
 
@@ -9,16 +8,4 @@ menuOpenBtn.addEventListener("click", () => {
 
 menuCloseBtn.addEventListener("click", () => {
     menu.classList.remove("show")
-})
-
-themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark")
-
-    if(themeBtn.classList.contains("ri-sun-line")){
-        themeBtn.classList.remove("ri-sun-line")
-        themeBtn.classList.add("ri-moon-line")
-    } else {
-        themeBtn.classList.remove("ri-moon-line")
-        themeBtn.classList.add("ri-sun-line")
-    }
 })
